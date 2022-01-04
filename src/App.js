87c1b65cb704c9a89ba43f6ae38d6f163150423e
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-04 18:36:24 +1000

    customise landing page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const BUILDSPACE_TWITTER_HANDLE = '_buildspace';
+const BUILDSPACE_TWITTER_LINK = `https://twitter.com/${BUILDSPACE_TWITTER_HANDLE}`;
+const TWITTER_HANDLE = 'redditech';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
 
 const App = () => {
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚔️ Stickman Battles ⚔️</p>
+          <p className="sub-text">Team up to fight the Big Boss!</p>
           <div className="connect-wallet-container">
             <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
+              src="https://cloudflare-ipfs.com/ipfs/QmYTW85JiTf75Yw2QNvCJP2Hyakg6fNTsyyhFzCdJDM52P"
+              alt="Red Shirt Hell with Phaser"
             />
           </div>
         </div>
@@ -27,7 +30,16 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
+          <span className="footer-text"> &nbsp; for my project on &nbsp;</span>
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={BUILDSPACE_TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`@${BUILDSPACE_TWITTER_HANDLE}`}</a>
+          <span className='footer-text'>&nbsp;:&nbsp;"Create your own mini turn-based NFT browser game"</span>
         </div>
       </div>
     </div>
