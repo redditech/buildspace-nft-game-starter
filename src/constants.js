e9eff61f9ee5ca61b48b10f02537e17c19afa6f9
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-05 07:23:17 +1000

    update contract address which has  IPFS hash only for imageURI

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
-const CONTRACT_ADDRESS = "0x01fDB4B43cE442452F887b695CA785d059ac64A2";
+const CONTRACT_ADDRESS = "0x0a4b6c7b25005d3B9a2E0C9F582821c7175976e6";
 
 const transformCharacterData = (characterData) => {
     return {
         name: characterData.name,
-        imageURI: characterData.imageURI,
+        imageURI: "https://cloudflare-ipfs.com/ipfs/" + characterData.imageURI,
         weapon: characterData.weapon,
         hp: characterData.hp.toNumber(),
         maxHp: characterData.maxHp.toNumber(),
