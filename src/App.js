7437b105df4b8e839d94318a37ea3279a23eb33e
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-04 18:48:03 +1000

    adds connect wallet function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -10,6 +10,60 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 
 const App = () => {
+
+  // State
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have Metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (accounts.length != 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account: ", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized account found!");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Implement your connectWallet method here
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      // request access to account
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      // This should print out the public address once we authorize Metamask.
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
   return (
     <div className="App">
       <div className="container">
@@ -21,6 +75,9 @@ const App = () => {
               src="https://cloudflare-ipfs.com/ipfs/QmYTW85JiTf75Yw2QNvCJP2Hyakg6fNTsyyhFzCdJDM52P"
               alt="Red Shirt Hell with Phaser"
             />
+            <button className="cta-button connect-wallet-button" onClick={connectWalletAction} >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
